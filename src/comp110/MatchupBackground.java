3df8c1997bfceaa62587f1620873f563fbf0485c
Stephen Firrincieli <sfirrincieli@gmail.com>
2016-02-25 21:23:15 -0500

    Do a fresh scrape of teams, this time in alphabetical order

--- src/comp110/MatchupBackground.java
+++ src/comp110/MatchupBackground.java
@@ -1,6 +1,7 @@
 package comp110;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import com.google.gson.Gson;
@@ -9,15 +10,16 @@ import com.google.gson.stream.JsonReader;
 public class MatchupBackground {
 
 	public static void main(String[] args) throws IOException {
-//	  // The scraping here only needs to be done once and takes about a minute per team
-//		// These are currently all of the ACC teams for testing and have been scraped to acc.json as of 02-06
-//		int[] teamIds = {67, 147, 193, 234, 255, 367, 415, 457, 490, 513, 545, 688, 746, 742, 749,
-//		    739, 328, 522, 812, 416, 312, 29, 529, 768, 334, 311, 306, 51, 559, 697, 732, 663, 703};
-//		// The rankingPeriod is there to be put into the ncaa website URLs
-//		// It changes periodically so make sure you have the current one before scraping
-//		// or else you will get outdated stats
-//		int rankingPeriod = 113;
-//		Scraper.saveToJSON("accplustop25.json", teamIds, rankingPeriod);
+	  // The scraping here only needs to be done once and takes about a minute per team
+		// These are currently all of the ACC teams for testing and have been scraped to acc.json as of 02-06
+		int[] teamIds = {67, 147, 193, 234, 255, 367, 415, 457, 490, 513, 545, 688, 746, 742, 749,
+		    739, 328, 522, 812, 416, 312, 29, 529, 768, 334, 311, 306, 51, 559, 697, 732, 663, 703};
+		Arrays.sort(teamIds);
+		// The rankingPeriod is there to be put into the ncaa website URLs
+		// It changes periodically so make sure you have the current one before scraping
+		// or else you will get outdated stats
+		int rankingPeriod = 114;
+		Scraper.saveToJSON("accplustop25.json", teamIds, rankingPeriod);
 		
 //		JsonReader reader = new JsonReader(new FileReader("acc.json"));
 //		Gson gson = new Gson();
