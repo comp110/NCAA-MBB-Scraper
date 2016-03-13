d4a8df230f1db8411dda7642a65906cedbcfc37a
Kris Jordan <krisjordan@gmail.com>
2016-03-13 12:02:45 -0400

    Update Bracket Runner

--- src/comp110/RunBracket.java
+++ src/comp110/RunBracket.java
@@ -1,78 +1,70 @@
 package comp110;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Map;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class RunBracket {
-  
+
   private static Map<Integer, Team> _teamMap;
 
   public static void main(String[] args) throws IOException {
-    
-       _teamMap = Base.readJSON();
-       BufferedReader csvReader = new BufferedReader(new FileReader("test.csv"));
-       // Skipping the first line to avoid parsing the headings
-       csvReader.readLine();       
-       
-       ArrayList<Game> gamesList = getGames(csvReader);
-       Game[] gamesArray = new Game[gamesList.size()];
-       gamesArray = gamesList.toArray(gamesArray);
-       
-       Student stu = new Student(gamesArray);
-       System.out.println("Total points: " + stu.getOverallScore());
-       System.out.println("Correct predictions: " + stu.getCorrect());
-       System.out.println("Incorrect preditions: " + stu.getIncorrect());
-       
-       saveToJSON(stu, "student_test.json");
-       
+    _teamMap = Base.readJSON();
+
+    String file = "";
+    if (args.length > 0) {
+      file = args[0];
+    } else {
+      System.err.println("Usage: comp110.RunBracket [csv]");
+      System.exit(1);
+    }
+    String games = args[0];
+    BufferedReader csvReader = new BufferedReader(new FileReader(file));
+    csvReader.readLine();
+
+    ArrayList<Game> gamesList = getGames(csvReader);
+    Game[] gamesArray = new Game[gamesList.size()];
+    gamesArray = gamesList.toArray(gamesArray);
+
+    printJSON(new Student(gamesArray));
   }
-  
+
   private static ArrayList<Game> getGames(BufferedReader csvReader) throws IOException {
-    // Reading in each line of the csv, instantiating a game for the line, and adding it to games
+    // Reading in each line of the csv, instantiating a game for the line, and
+    // adding it to games
     String line = "";
     ArrayList<Game> games = new ArrayList<>();
     while ((line = csvReader.readLine()) != null) {
       String[] splitCsv = line.split(",");
-      
-      // Accounting for text csv lines we can use for comments or to indicate a new round
+
+      // Accounting for text csv lines we can use for comments or to indicate a
+      // new round
       try {
         Integer.parseInt(splitCsv[0]);
       } catch (NumberFormatException nfe) {
         continue;
       }
-      
+
       int[] csvInt = new int[splitCsv.length];
       // Converting to int[]
-      for (int i=0; i<splitCsv.length; i++) {
+      for (int i = 0; i < splitCsv.length; i++) {
         csvInt[i] = Integer.parseInt(splitCsv[i]);
       }
       games.add(new Game(csvInt[0], csvInt[1], csvInt[2], csvInt[3], csvInt[4], csvInt[5]));
     }
-    
+
     return games;
   }
-  
-  private static void saveToJSON(Student student, String filename) throws IOException {
-    File outFile = new File(filename);
-    FileOutputStream outStream = new FileOutputStream(outFile);
-    OutputStreamWriter outWriter = new OutputStreamWriter(outStream, "UTF-8");
-    // Pretty printing for when you need to look at the json
-    // Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    Gson gson = new Gson();
-    gson.toJson(student, outWriter);
-    outWriter.close();
+
+  private static void printJSON(Student student) {
+    System.out.println("\n\n");
+    System.out.println(new Gson().toJson(student));
   }
-  
+
   public static Team getTeam(int id) {
     return _teamMap.get(id);
   }
